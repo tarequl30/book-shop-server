59ed4243b596122d811832a2840dfa2d751ad0e2
Tareq <tarequl30@gmail.com>
2021-04-06 05:13:09 +0600

    updated

--- index.js
+++ index.js
@@ -15,11 +15,20 @@ const uri = "mongodb+srv://heroShop:heroShop79@cluster0.5gken.mongodb.net/heroSh
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("heroShop").collection("books");
+
+  app.get('/books', ( req,res ) => {
+      collection.find()
+      .toArray((err, items) => {
+        res.send(items)
+        console.log(items)
+      })
+  })
+
   app.post('/addBooks', (req, res) => {
     const product = req.body
     collection.insertOne(product)
     .then(result => {
-        res.send(result.insertedProduct > 0)
+        res.send(result.insertedCount > 0)
     })
   }) 
 });
